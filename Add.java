15b6e76a6037b48bce3706f919c6b38818761122
srm <srm@ubuntuhondir1.msubaroda.ac.in>
2011-10-16 14:30:19 +0530

    now I am adding three numbers

--- Add.java
+++ Add.java
@@ -4,10 +4,12 @@ public class Add {
 	private int i;
 	private int j;
 	private int k;
+	private int l;
 	public static void main (String [] args) {
 		int i = Integer.parseInt(args [0]);
 		int j = Integer.parseInt(args [1]);
-		int k = i+j;
-		System.out.println("The addition of "+i+" & " + j+ " is = " +k);
+		int l = Integer.parseInt(args [2]);
+		int k = i+j+l;
+		System.out.println("The addition of "+i+" , "+j+" & " + l+ " is = " +k);
 	}
 }
