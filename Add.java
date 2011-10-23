d86ce0e21cda76f901576f59f8b443f161eeaef2
Dipak Shukla <dns.shukla@gmail.com>
2011-10-23 15:05:21 +0530

    now i am adding 5 numbers

--- Add.java
+++ Add.java
@@ -5,13 +5,15 @@ public class Add {
 	private int j;
 	private int k;
 	private int l;
+	private int o;
         private int m;
 	public static void main (String [] args) {
 		int i = Integer.parseInt(args [0]);
 		int j = Integer.parseInt(args [1]);
 		int l = Integer.parseInt(args [2]);
                 int m = Integer.parseInt(args [3]);
-		int k = i+j+l+m;
-		System.out.println("The addition of "+i+" , "+j+" , " + l+" & "+m+ " is = " +k);
+		int o = Integer.parseInt(args [4]);
+		int k = i+j+l+m+o;
+		System.out.println("The addition of "+i+" , "+j+" , " + l+" & "+m+" , "+o+" is = " +k);
 	}
 }
